43c8d811161e16cfff6d4c10ee82d73ffe0feb3f
David Carton <cartondj@gmail.com>
2017-07-18 16:05:17 +0200

    bug fix mixing x and y coords

--- galaxy.py
+++ galaxy.py
@@ -848,7 +848,7 @@ class GalaxyMap(BaseGalaxy):
         
         #store map coords and pixel area
         self.x = x
-        self.y = x
+        self.y = y
         self.bin_area = area
         self.bin_coord = np.column_stack([x, y])
 
